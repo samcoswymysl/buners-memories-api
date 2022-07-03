f5dea65fc090c8259670e7860f3a6e8898b341a3
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2022-07-03 15:58:48 +0200

    Added connect to mongoDB and congig file

--- server.ts
+++ server.ts
@@ -1,17 +1,28 @@
 import express from 'express'
 import 'express-async-errors'
-import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import cors from 'cors'
+import {MONGO_URL, PORT} from "./config/config";
+import mongoose from "mongoose";
 
-const server = express()
+(async ()=> {
+
+    const server = express();
+    server.use(bodyParser.json({limit: '30mb'}));
+    server.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+    server.use(cors());
+
+
+    try {
+        await mongoose.connect(MONGO_URL)
+        server.listen(PORT as number, 'localhost', ()=> console.log(`Server listen on port ${PORT}`))
+    }
+    catch (e) {
+        console.log(e)
+    }
+    })();
 
 
 
-server.use(bodyParser.json({limit: '30mb', extended: true}))
-server.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
-server.use(cors())
 
 
-
-server.listen(5000, 'localhost', ()=> console.log('Server listen on port 5000'))
