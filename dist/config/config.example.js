e818b0e44622c1985d6401d38cb4805baef4bbed
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2022-07-12 19:12:32 +0200

    Delete dist dirrecotry

--- dist/config/config.example.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// export const MONGO_URL = 'Url to connect with mongoDB'
-// export const PORT = process.env.PORT  || 5000
-//# sourceMappingURL=config.example.js.map
--- dist/config/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGO_URL = void 0;
-exports.MONGO_URL = 'mongodb+srv://buners2:zaq12wsx@memo.2zyqvxy.mongodb.net/?retryWrites=true&w=majority';
-//# sourceMappingURL=config.js.map
